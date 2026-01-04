71e132362fd3784b6d9f7a6a3adc1e2a7b868b1a
Yasmin Ayuni <ysmnayn04@gmail.com>
2026-01-04 12:48:18 +0800

    Testing conflicts updated setLocation

--- CustomerDetails.java
+++ CustomerDetails.java
@@ -16,7 +16,7 @@ public class CustomerDetails extends JFrame implements ActionListener{
         super("Customer Details");
         
         setSize(1200,650); // Comment by Yasmin - testing Git collaboration
-        setLocation(200,150);
+        setLocation(300,150); // Testing conflicts - Updated setLocation
         
         table = new JTable();
         
