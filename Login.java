145a2d75b86a8b64b2fe7052c5288757511bd2d5
Hafiy <hafiyazfar05@gmail.com>
2026-01-04 14:10:32 +0800

    testing conflict

--- Login.java
+++ Login.java
@@ -15,7 +15,7 @@ public class Login extends JFrame implements ActionListener{
     Login(){
         super("Login Page");
         getContentPane().setBackground(Color.white);
-        System.out.print("Testing conflicts");
+        System.out.print("Hellow World login Page");
         setLayout(null);
         
         JLabel lblusername = new JLabel("Username");
